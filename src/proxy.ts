1ae48057bdfd6f2539d68c28a64177099b5f25ea
Sarvesh7617 <shubhamsoni1231@rediffmail.com>
2026-04-27 13:57:02 +0530

    Update proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -36,7 +36,7 @@ export default proxy;
 export const config = {
   matcher: [
     // Exclude API routes, static files, image optimizations, and .png files
-    '/((?!api|_next/static|_next/image|favicon.ico|node_moduel|.*\\.png$).*)',
+    '/((?!api|_next/static|_next/image|favicon.ico|node_module|.*\\.png$).*)',
     '/dashboard'
   ],
 }
